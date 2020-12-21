5fe362b99f741f3e78062f95453c8b90776017a1
dayuansari <ranisa28@gmail.com>
2020-12-21 21:43:55 +0700

    materi

---
+++ public/js/materi-dasar.js
@@ -0,0 +1,16 @@
+let materi = 'abjad';
+
+fetch('/data/materi-dasar.json').then
+(rest => rest.json()).then (data => {
+console.log(data.materidasar);
+
+switch(materi){
+    case 
+}
+} )
+
+
+
+function deskripsi (deskripsi){
+    
+}

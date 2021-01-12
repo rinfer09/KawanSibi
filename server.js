d16fb2b9adf349c1a195ab6f58eae3f9f3cbc01b
Dennis Ferdian <cungfochai@gmail.com>
2021-01-12 15:58:40 +0700

    add cookie parser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
